ea6714680b6f0e8d80a8c17e40157fb402bc41ca
root <root@ip-172-31-32-112.ap-south-1.compute.internal>
2022-11-04 10:25:32 +0000

    terra commits

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "example" {
+ for_each = {
+    host-1= "ansible"
+    host-2= "webapp"
+}
+  ami           = "ami-0e6329e222e662a52"
+  instance_type = "t2.medium"
+  tags= {
+    Name = "${each.key}" 
+  }
+}
+
