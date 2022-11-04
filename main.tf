a323b83fc348161a5ddc84a8e82fc83de7d62962
RAHAMSHAIK007 <81603735+RAHAMSHAIK007@users.noreply.github.com>
2022-11-04 16:04:31 +0530

    terraa commits

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "example" {
  for_each = {
     host-1= "ansible"
-    host-2= "webapp"
+    host-2= "Docker"
 }
   ami           = "ami-0e6329e222e662a52"
   instance_type = "t2.medium"
